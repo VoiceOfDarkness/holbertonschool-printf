5e823b96e9ef3c1e77066c119775f2c75aff0e8c
Abilhasan Samadov <7418@holbertonstudents.com>
2023-11-28 04:29:27 -0800

    main.h

---
+++ main.h
@@ -0,0 +1,22 @@
+#ifndef PRINT_F
+#define PRINT_F
+
+#include <stdio.h>
+#include <stdarg.h>
+#include <stdlib.h>
+
+int print_string(va_list);
+int print_char(va_list);
+
+/**
+ * struct print - struct for printing
+ * @type: type of data
+ * @f: function to print
+ */
+typedef struct _format
+{
+        char type;
+	int (*f)(va_list);
+} format_t;
+
+#endif // !PRINT_F
